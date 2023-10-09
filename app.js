c7b06528e743ff621be773d82ac494741180ece6
John Clampet <jsclampet@gmail.com>
2023-10-09 07:55:30 -0500

    WIP; added some functionality; form-validation via JS, need to complete; need to remove 'required' tags from HTML

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const expenseDate = document.getElementById("date");
 const expenseAmount = document.getElementById("amount");
 //tested all EXCEPT "expenseDate" , everything checks out so far
 
+let addButton = document.getElementById("add-button");
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  formValidation();
+});
 //TABLE CELLS
 // testing
 const tableBody = document.getElementById("table-body");
@@ -13,6 +18,20 @@ tableRow.innerHTML = `
   <td>test</td>
   <td>asdfasdf asdfasdf asf asfd </td>
   <td>test</td>
-  <td>test</td>
+  <td>123123</td>
 `;
 tableBody.append(tableRow);
+
+function formValidation() {
+  let error = document.getElementById("error");
+
+  let nameVal = document.forms["expense-form"]["name"].value;
+  let dateVal = document.forms["expense-form"]["date"].value;
+  let amountVal = document.forms["expense-form"]["amount"].value;
+
+  if (amountVal == "" || amountVal == null) {
+    error.classList.remove("hidden");
+  } else {
+    error.classList.add("hidden");
+  }
+}

7652bbd32017edb408a954fe0bd0dc4af94dc83c
John Clampet <jsclampet@gmail.com>
2023-10-09 23:39:19 -0500

    final draft; added delete button along with more formatting for date

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const paymentType = document.getElementById("paymentType");
 const expenseName = document.getElementById("name");
 const expenseDate = document.getElementById("date");
 const expenseAmount = document.getElementById("amount");
-//tested all EXCEPT "expenseDate" , everything checks out so far
 
 let addButton = document.getElementById("add-button");
 addButton.addEventListener("click", (e) => {
@@ -19,18 +18,102 @@ const paymentIcons = {
   other: `<i class="fa-solid fa-question"></i>`,
 };
 
-//TABLE CELLS
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const tablePlaceholder = document.getElementById("table-placeholder");
 const tableBody = document.getElementById("table-body");
 
+//
+//
+// DEMO / TESTING
+
+// const tableRowTest = document.createElement("tr");
+// tableRowTest.innerHTML = `
+//   <td>${paymentIcons.card}</td>
+//   <td>asdfasf</td>
+//   <td>${new Date()}</td>
+//   <td>1222</td>
+//   <button >❌</button>
+// `;
+// tableBody.append(tableRowTest);
+
+// DEMO / TESTING
+//
+//
+
 function onFormSubmit() {
+  tablePlaceholder.classList.add("hidden");
+
   const tableRow = document.createElement("tr");
+  tableRow.classList.add("expense-row");
   tableRow.innerHTML = `
-    <td>${paymentIcons[paymentType.value]}</td>
+    <td><a href="#"><i class="fa-solid fa-x close-icon"></i></a>${
+      paymentIcons[paymentType.value]
+    }</td>
     <td>${expenseName.value}</td>
-    <td>${expenseDate.value}</td>
-    <td>${expenseAmount.value}</td>
+    <td>${reformattedDate()}</td>
+    <td>$${expenseAmount.value}</td>
   `;
   tableBody.append(tableRow);
+  // clear form input values
+  expenseName.value = "";
+  expenseDate.value = "";
+  expenseAmount.value = "";
+
+  // remove expense
+  const closeBtn = document.querySelectorAll(".close-icon");
+  closeBtn.forEach((button, index) => {
+    button.addEventListener("click", (e, i) => {
+      let expenseRows = document.querySelectorAll(".expense-row");
+      expenseRows[index].remove();
+      // if no more expenses, display placeholder message
+      if (expenseRows.length < 2) {
+        tablePlaceholder.classList.remove("hidden");
+      }
+    });
+  });
+}
+
+function reformattedDate() {
+  let dateArray = expenseDate.value.split("-");
+  dateArray.shift();
+  let mappedDate = dateArray.map((item, i) =>
+    i === 0 ? months[item - 1] : item
+  );
+  let month = mappedDate[0];
+  let dayDigits = mappedDate[1].split("");
+  console.log(dayDigits);
+  let day = dayDigits[0] === "0" ? dayDigits[1] : dayDigits.join("");
+
+  // add suffix
+  switch (dayDigits[1]) {
+    case "1":
+      day = day + "st";
+      break;
+    case "2":
+      day = day + "nd";
+      break;
+    case "3":
+      day = day + "rd";
+      break;
+    default:
+      day = day + "th";
+  }
+
+  return `${month} ${day}`;
 }
 
 function formValidation() {
